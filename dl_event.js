149ea8b5a3d99115e582a011102fa24c7f990eac
XinyingZou <XinyingZou@users.noreply.github.com>
2018-03-29 17:30:23 +0800

    Update dl_event.js

--- dl_event.js
+++ dl_event.js
@@ -17,10 +17,12 @@ ga_vs_tracking.header_click = function(){
    });
 };
 ga_vs_tracking.footer_navigation_click = function() {
+  var arr = ['homepage','product detail','product list'];
+  var index = Math.floor((Math.random()*arr.length));
   window.dataLayer.push({
     'event': 'GA-event',
     'eventDetail': {
-        'category': 'homepage', 
+        'category': arr[index], 
         'action': 'footer',
         'label': undefined
     },
@@ -29,35 +31,47 @@ ga_vs_tracking.footer_navigation_click = function() {
 }
 
 ga_vs_tracking.first_navigation_click = function() {
+  var arr = ['homepage','product detail','product list'];
+  var index = Math.floor((Math.random()*arr.length));
+  var arr2 = ['firstnav-文胸','firstnav-内裤','firstnav-舒适睡衣'];
+  var index2 = Math.floor((Math.random()*arr2.length));
   window.dataLayer.push({
     'event': 'GA-event',
     'eventDetail': { 
-      'category': 'homepage', 
+      'category': arr[index],  
       'action': 'navigation',
-      'label': 'firstnav-文胸'
+      'label': 'arr2[index2]
     },
 	'ecommerce': undefined
  });
 }
 
 ga_vs_tracking.second_navigation_click = function() {
+  var arr = ['homepage','product detail','product list'];
+  var index = Math.floor((Math.random()*arr.length));
+  var arr2 = ['secnav-内裤-款式-半包臀内裤','secnav-文胸-all','secnav-文胸-效果-超薄'];
+  var index2= Math.floor((Math.random()*arr2.length));
   window.dataLayer.push({
     'event': 'GA-event',
     'eventDetail': { 
-      'category': 'homepage', 
+      'category': arr[index], 
       'action': 'navigation',
-      'label': 'secnav-内裤-款式-半包臀内裤'
+      'label': arr2[index2]
     },
 	'ecommerce': undefined
  });
 }
 
 ga_vs_tracking.product_category_click = function() {
+  var arr = ['homepage','product detail','product list'];
+  var index = Math.floor((Math.random()*arr.length));
+  var arr2 = ['kv','section','今日特惠'];
+  var index2 = Math.floor((Math.random()*arr2.length));
   window.dataLayer.push({
     'event': 'GA-event',
     'eventDetail': { 
-        'category': 'homepage',
-        'action': 'kv',
+        'category': arr[index],
+        'action': arr2[index2],
         'label': 'dream angels wicked'
     },
 	'ecommerce': undefined
@@ -65,12 +79,18 @@ ga_vs_tracking.product_category_click = function() {
 }
 
 ga_vs_tracking.product_click =  function() {
+  var arr = ['homepage','product detail','product list'];
+  var index = Math.floor((Math.random()*arr.length));
+  var arr2 = ['kv','section','今日特惠'];
+  var index2 = Math.floor((Math.random()*arr2.length));
+  var arr3 = ['quick view','normal'];
+  var index3 = Math.floor((Math.random()*arr3.length));
   window.dataLayer.push({
       'event': 'GA-event',
       'eventDetail': { 
-        'category': 'product detail', 
-        'action': 'section-精选搭配',
-        'label': 'quick view'
+        'category': arr[index],
+        'action': arr2[index2],
+        'label': arr3[index3]
       },
 	  'ecommerce': {
         'click': {
@@ -88,11 +108,15 @@ ga_vs_tracking.product_click =  function() {
 }
 
 ga_vs_tracking.product_search = function() {
+  var arr = ['homepage','product detail','product list'];
+  var index = Math.floor((Math.random()*arr.length));
+  var arr2 = ['黑色文胸','运动文胸','条纹睡衣'];
+  var index2 = Math.floor((Math.random()*arr2.length));
   window.dataLayer.push({
     'event': 'GA-event',
     'eventDetail': { 
-        'category': 'homepage',
-        'action': 'search',
+        'category': arr[index],
+        'action': arr2[index2],
         'label': '黑色文胸'
     },
 	'ecommerce': undefined
@@ -134,12 +158,14 @@ ga_vs_tracking.user_logout =  function() {
 
 //category == chat
 ga_vs_tracking.online_chat_conversation_start =  function() {
+  var arr = ['homepage','product detail','product list'];
+  var index = Math.floor((Math.random()*arr.length));
   window.dataLayer.push({
     'event': 'GA-event',
     'eventDetail': {
       'category': 'chat', 
       'action': 'start',
-      'label': 'header'
+      'label': arr[index]
     },
 	'ecommerce': undefined
   });
@@ -147,12 +173,14 @@ ga_vs_tracking.online_chat_conversation_start =  function() {
 
 //category == search
 ga_vs_tracking.search_no_results = function() {
+  var arr = ['ABCDEFG','运动长裤','大秀手袋'];
+  var index = Math.floor((Math.random()*arr.length));
   window.dataLayer.push({
      'event': 'GA-event',
      'eventDetail': { 
         'category': 'search no results', 
         'action': 'no results',
-        'label': 'ABCDEFG'
+        'label': arr[index]
       },
 	  'ecommerce': undefined
    });
@@ -160,12 +188,16 @@ ga_vs_tracking.search_no_results = function() {
 
 //category == product filter
 ga_vs_tracking.product_filter =  function() {
+  var arr = ['文胸','内裤','舒适睡衣'];
+  var index = Math.floor((Math.random()*arr.length));
+  var arr2 = ['款式-3|4罩杯','颜色-黑色','颜色-条纹']
+  var index2 = Math.floor((Math.random()*arr2.length));
   window.dataLayer.push({
      'event': 'GA-event',
      'eventDetail': { 
         'category': 'product filter', 
-        'action': '文胸',
-        'label': '款式-3|4罩杯'
+        'action': arr[index],
+        'label': arr2[index2]
       },
 	  'ecommerce': undefined
    });
@@ -173,12 +205,14 @@ ga_vs_tracking.product_filter =  function() {
 
 //category == ecommerce
 ga_vs_tracking.add_to_shopping_cart =  function() {
+  var arr = ['quick view','quick view-猜您喜欢','product detail'];
+  var index = Math.floor((Math.random()*arr.length));
   window.dataLayer.push({
      'event': 'GA-event',
      'eventDetail': { 
         'category': 'ecommerce', 
         'action': 'add to cart',
-        'label': 'quick view',
+        'label': arr[index]
       },
 	  'ecommerce': {
 		  'add' : {
@@ -221,12 +255,14 @@ ga_vs_tracking.remove_from_shopping_cart =  function() {
 }
 
 ga_vs_tracking.checkout_order_submit =  function() {
+  var arr = ['member','guest'];
+  var index = Math.floor((Math.random()*arr.length));
   window.dataLayer.push({
      'event': 'GA-event',
      'eventDetail': { 
         'category': 'ecommerce', 
         'action': 'order submit',
-        'label': 'member'
+        'label': arr[index]
       },
 	  'ecommerce': {
 		  'checkout': {
@@ -290,12 +326,16 @@ ga_vs_tracking.payment_unsuccessful = function() {
 }
 
 ga_vs_tracking.refund_and_return_confirm = function() {
+  var arr = ['refund','return'];
+  var index = Math.floor((Math.random()*arr.length));
+  var arr2 = ['不想要了','尺码不合适'];
+  var index2 = Math.floor((Math.random()*arr2.length));
   window.dataLayer.push({
     'event': 'GA-event',
     'eventDetail': { 
       'category': 'ecommerce', 
-      'action': 'refund',
-      'label': '不想要了',
+      'action': arr[index],
+      'label': arr2[index2]
     },
     'ecommerce': undefined
  });
